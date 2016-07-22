1d86114ef92b1b952998128fac7cbeac845f278f
Henryk Wollik <olafyoorsen@hotmail.com>
2016-07-22 20:06:01 +0200

    Keyboard doc++

--- KeyboardEvent.js
+++ KeyboardEvent.js
@@ -1,11 +1,31 @@
 import Event from 'foam-event/Event';
 
+/**
+ * KeyboardEvent
+ * @class KeyboardEvent
+ * @classdesc Keyboard input event representation.
+ * @augments Event
+ */
 export default class KeyboardEvent extends Event{
     constructor(type,data){
         super(type,data);
     }
 }
 
+/**
+ * Key event type key press.
+ * @type {string}
+ */
 KeyboardEvent.KEY_PRESS = 'keypress';
+
+/**
+ * Key event type key down.
+ * @type {string}
+ */
 KeyboardEvent.KEY_DOWN = 'keydown';
+
+/**
+ * Key event type key up.
+ * @type {string}
+ */
 KeyboardEvent.KEY_UP = 'keyup';
