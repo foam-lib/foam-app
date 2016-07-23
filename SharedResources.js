ba8af45c960a59516de0efc5b371599fcdf66336
Henryk Wollik <olafyoorsen@hotmail.com>
2016-07-23 22:37:38 +0200

    SharedResources doc initial

--- SharedResources.js
+++ SharedResources.js
@@ -1,4 +1,9 @@
-let SharedResources = {};
+/**
+ * Resources loaded on app startup shared across the application.
+ * @type {object}
+ * @property {function} dispose - Disposes all loaded resources.
+ */
+const SharedResources = {};
 
 SharedResources.dispose = function(){
     for(let key in this){
