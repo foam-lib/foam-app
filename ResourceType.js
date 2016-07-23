65ec80ae7f426738471e11347cdc53d48840e688
Henryk Wollik <olafyoorsen@hotmail.com>
2016-07-23 22:31:56 +0200

    ResourceType doc initial

--- ResourceType.js
+++ ResourceType.js
@@ -1,9 +1,19 @@
-const ResourceType = {
+/**
+ * Resource type representation.
+ * @readonly
+ * @enum {string}
+ * @property {string} TEXT - Text resource
+ * @property {string} IMAGE - Image resource
+ * @property {string} VIDEO - Video resource
+ * @property {string} JSON - JSON resource
+ * @property {string} ARRAY_BUFFER - ArrayBuffer resource
+ */
+const ResourceType = Object.freeze({
     TEXT : 'text',
     IMAGE : 'image',
     VIDEO : 'video',
     JSON : 'json',
     ARRAY_BUFFER : 'arraybuffer'
-};
+});
 
 export default ResourceType;
